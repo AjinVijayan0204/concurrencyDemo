0ce993755d835f3ea3402a42573669b52b9b447b
AjinVijayan0204 <ajinvijayan0204@gmail.com>
2022-07-10 16:49:13 +0530

    added parallel

--- AyncTestCheck/ViewController.swift
+++ AyncTestCheck/ViewController.swift
@@ -12,8 +12,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       doSomething()
+        
     }
 
 
 }
+func doSomething() {
+    
+    Task {
+        // Create & start a child task
+        async let a = performTaskA()
 
+        // Create & start a child task
+        async let b = performTaskB()
+
+        let sum = await (a + b)
+        print(sum) // Output: 5
+    }
+}
+func performTaskA() async -> Int {
+
+    // Wait for 2 seconds
+    //await Task.sleep(5 * 1_000_000_000)
+    await try? Task.sleep(nanoseconds: 5 * 1_000_000_000)
+    print("Adone")
+    return 2
+}
+
+func performTaskB() async -> Int {
+    
+    // Wait for 3 seconds
+//    await Task.sleep(2 * 1_000_000_000)
+    await try? Task.sleep(nanoseconds: 2 * 1_000_000_000)
+    print("Bdone")
+    return 3
+}
